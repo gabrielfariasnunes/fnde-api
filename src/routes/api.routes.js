eaf4962329fec1e6cb650d4ac7b5b3912aac9d4d
Gabriel Farias Nunes <gabrielfariasdp@gmail.com>
2025-05-31 19:29:48 -0300

    update

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,7 +1,7 @@
 const routes = require("express").Router();
 const { getPaymentsController } = require("../controllers/api.controllers");
 
-routes.get("/v1/pagamentos/:cpf", getPaymentsController);
+routes.get("/api/pagamentos/:cpf", getPaymentsController);
 routes.use((req, resp, next) => {
     resp.end()
 })
