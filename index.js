0de89c0637d04b14220d859d865e6358bf521ece
Gabriel Farias Nunes <gabrielfariasdp@gmail.com>
2025-05-31 18:26:26 -0300

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require("express")();
+const compression = require("compression")
+const helmet = require("helmet")
+
+const  { environments } = require("./src/config/env");
+const { routes } = require("./src/routes/api.routes");
+
+app.use(compression());
+app.use(helmet())
+app.use(routes)
+
+app.listen(environments.server.port, () => {
+    console.log(`Server running on port ${environments.server.port}`)
+})
